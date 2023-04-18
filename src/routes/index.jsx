5cb6dbfe4539c686087b529c1e1c6766f76592cb
alamincoders <developeralamin03@gmail.com>
2023-04-18 10:42:04 +0600

    bug fix

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,13 @@
 import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
-import Layout from "../layouts/Layout";
-import Error from "../pages/Error";
-import Dashboard from "../pages/Dashboard";
-import Products from "../pages/Products";
-import Customers from "../pages/Customers";
-import Table from "../pages/Table";
-
 const Layout = lazy(() => import("../layouts/Layout"));
 const Erro = lazy(() => import("../pages/Error"));
 const Dashboard = lazy(() => import("../pages/Dashboard"));
 const Products = lazy(() => import("../pages/Products"));
 const Customers = lazy(() => import("../pages/Customers"));
 const Form = lazy(() => import("../components/lib/Form"));
-
+const Table = lazy(() => import("../pages/Table"));
 
 const router = createBrowserRouter([
   {
@@ -49,6 +42,5 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-
 
 export default router;
